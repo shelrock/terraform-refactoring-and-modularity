6d987946a7c22af49482c0324c25cdbb36183ea1
terratester <shelrock79@gmail.com>
2026-07-14 23:26:06 +0900

    code refactoring

--- 0830_iam.tf
+++ 0830_iam.tf
@@ -8,6 +8,15 @@ resource "aws_iam_group_policy_attachment" "policy_attach_to_CloudArchitectureTe
   policy_arn = var.CloudArchitectureTeam_policy
 }
 
+# resource "aws_iam_user" "CloudArchitectureTeam_users" {
+#   count = length(var.CloudArchitectureTeam_users)
+#   name  = element(var.CloudArchitectureTeam_users, count.index)
+
+#   tags = {
+#     Name = "CloudArchitectureTeam_user"
+#   }
+# }
+
 resource "aws_iam_user" "CloudArchitectureTeam_users" {
   count = length(var.CloudArchitectureTeam_users)
   name  = element(var.CloudArchitectureTeam_users, count.index)
@@ -16,6 +25,9 @@ resource "aws_iam_user" "CloudArchitectureTeam_users" {
     Name = "CloudArchitectureTeam_user"
   }
 }
+
+
+
 
 resource "aws_iam_user_group_membership" "CloudArchitectureTeam_membership" {
   count = length(var.CloudArchitectureTeam_users)
